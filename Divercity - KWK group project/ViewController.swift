d057d01dd2b764ba18f9bba9e8403f9b2142040f
Rachel Gunawan <rochitapiglet@gmail.com>
2020-07-14 13:33:53 -0700

    Initial Commit

---
+++ Divercity - KWK group project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Divercity - KWK group project
+//
+//  Created by Rachel Tiffany Gunawan on 7/14/20.
+//  Copyright © 2020 Rachel Tiffany Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
