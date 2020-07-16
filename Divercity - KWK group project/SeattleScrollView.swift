2b38af08e7dcc32a2e88baf157789972fc103dc3
Rachel Gunawan <rochitapiglet@gmail.com>
2020-07-16 11:12:27 -0700

    drop down menu semi works lol

---
+++ Divercity - KWK group project/SeattleScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  SeattleScrollView.swift
+//  Divercity - KWK group project
+//
+//  Created by Rachel Tiffany Gunawan on 7/16/20.
+//  Copyright © 2020 Rachel Tiffany Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class SeattleScrollView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
