b23bdf4f571e6c160ab10f1eef5873992a457004
Rachel Gunawan <rochitapiglet@gmail.com>
2020-07-16 10:48:29 -0700

    added drop down menu on seattle, but not appearing on the simulator

--- Divercity - KWK group project/SeattleViewController.swift
+++ Divercity - KWK group project/SeattleViewController.swift
@@ -10,12 +10,55 @@ import UIKit
 
 class SeattleViewController: UIViewController {
 
+    @IBOutlet weak var articleButton: UIButton!
+    @IBOutlet weak var articleLabel: UILabel!
+    
+    @IBOutlet weak var businessButton: UIButton!
+    @IBOutlet weak var businessLabel: UILabel!
+    
+    @IBOutlet weak var restaurantButton: UIButton!
+    @IBOutlet weak var restaurantLabel: UILabel!
+    
+    @IBAction func selectArticle(_ sender: Any) {
+        UIView.animate(withDuration: 0.3, animations: {
+            if self.articleLabel.isHidden{
+                self.movePosition(button: self.businessButton, label: self.businessLabel, yPosition: Double(self.articleLabel.frame.origin.y + self.articleLabel.frame.size.height))
+                self.movePosition(button: self.restaurantButton, label: self.restaurantLabel, yPosition: Double(self.restaurantButton.frame.origin.y + self.articleLabel.frame.size.height))
+            }else{
+                self.movePosition(button: self.businessButton, label: self.businessLabel, yPosition: 50.0)
+                self.movePosition(button: self.restaurantButton, label: self.restaurantLabel, yPosition: Double(self.restaurantButton.frame.origin.y - self.articleLabel.frame.size.height))
+            }
+        })
+        self.articleLabel.isHidden = !self.articleLabel.isHidden
+        self.view.layoutIfNeeded()
+    }
+    
+    @IBAction func selectBusiness(_ sender: Any) {
+        UIView.animate(withDuration: 0.3, animations: {
+            if self.businessLabel.isHidden{
+                self.movePosition(button: self.restaurantButton, label: self.restaurantLabel, yPosition: Double(self.restaurantButton.frame.origin.y + self.businessLabel.frame.size.height))
+            }else{
+                self.movePosition(button: self.restaurantButton, label: self.restaurantLabel, yPosition: Double(self.restaurantButton.frame.origin.y - self.businessLabel.frame.size.height))
+            }
+        })
+        self.businessLabel.isHidden = !self.businessLabel.isHidden
+        self.view.layoutIfNeeded()
+    }
+    
+    @IBAction func selectRestaurant(_ sender: Any) {
+        restaurantLabel.frame.origin.y = restaurantButton.frame.origin.y + 50.0
+        restaurantLabel.isHidden = !restaurantLabel.isHidden
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+    func movePosition(button : UIButton, label : UILabel, yPosition : Double){
+        button.frame = CGRect(x: 0.0, y: yPosition, width: 414.0, height: 50.0)
+        label.frame.origin.y = button.frame.origin.y + 50.0
+    }
 
     /*
     // MARK: - Navigation
